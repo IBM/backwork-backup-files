bd3d8a16e9a702670be1f813c301082da687817e
James Reeve <reevejamesd@gmail.com>
2019-08-17 09:19:02 -0400

    add support for restores

--- README.md
+++ README.md
@@ -12,8 +12,10 @@ $ pip install backwork-backup-files
 ```
 
 ## Using
-After installing the plug-in you will be able to use the `backup files` command
-on `backwork`.
+After installing the plug-in you will be able to use the `backup files` and `restore files` commands
+on `backwork`:
+
+#### backwork backup files
 
 ```sh
 $ backwork backup files -h
@@ -29,7 +31,7 @@ optional arguments:
   -o FILE, --output FILE  output gzipped file path
 ```
 
-You can pass any option that you would normally use on `mysqldump`:
+You can pass any option that you would normally use with `tar`:
 
 ```sh
 $ backwork backup files -o foo.tgz --verbose /tmp /var/log
@@ -40,6 +42,22 @@ must use in your backup process.
 
 `-o FILE` or `--output FILE` will save the output of `tar` into a
 file.
+
+#### backwork restore files
+
+```sh
+usage: backwork restore files [-h] input
+
+Restore up one or more files from a .tar.gz file. It uses `tar xvzf`. You can
+use any of the arguments supported by `tar`. Use `tar --help` for more
+information.
+
+positional arguments:
+  input       .tar.gz file to restore from
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
 
 **Important:** There is a conflict with the `-h` argument since it is reserved
 for the help/usage message. Use `--dereference` to follow symlinks.
--- files/files.py
+++ files/files.py
@@ -44,9 +44,48 @@ class FilesBackup(object):
         try:
             self.result = subprocess.check_output(
                 tar_cmd, stderr=subprocess.STDOUT)
-            LOGGER.info("output:\n\n\t%s", "\n\t".join(
-                self.result.split("\n")))
+            LOGGER.info(self.result.decode('utf-8'))
             LOGGER.info("file backup complete")
         except Exception as error:
             LOGGER.error("Failed to backup files")
+            raise error
+
+
+class FilesRestore(object):
+    """Restore up one or more files from a .tar.gz file.
+
+    It uses `tar xvzf`. You can use any of the
+    arguments supported by `tar`. Use `tar --help` for more information.
+    """
+    command = "files"
+
+    def __init__(self, args, extra):
+        """Initialize a restore command given the arguments passed to CLI"""
+
+        self.args = args
+        self.extra = extra
+        self.result = ""
+
+    @classmethod
+    def parse_args(cls, subparsers):
+        """Create the `files` subparser for the `restore` command.
+        :param subparsers: Main argument parser
+        """
+        fs_parser = subparsers.add_parser(cls.command, description=cls.__doc__)
+
+        fs_parser.add_argument("input", help=".tar.gz file to restore from")
+
+    def restore(self):
+        """Restore one or more files."""
+        LOGGER.info("starting file restore...")
+
+        tar_cmd = ["tar", "xvzf", self.args.input] + self.extra
+
+        try:
+            self.result = subprocess.check_output(
+                tar_cmd, stderr=subprocess.STDOUT)
+            LOGGER.info('\n'+self.result.decode('utf-8'))
+            LOGGER.info("file restore complete")
+        except Exception as error:
+            LOGGER.error("Failed to restore files")
             raise error
